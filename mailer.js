adef4cc0e7541c390d126ff69ae505755cb98af9
KiraAylaria <74116888+KiraAylaria@users.noreply.github.com>
2022-09-15 12:28:16 +0200

    Update mailer.js

--- mailer.js
+++ mailer.js
@@ -9,14 +9,17 @@ class Mailer {
     #port
     #secure
     #mailInfo = null
+    #blindCopy = null
+    #sender = null
     
     constructor(userName, host, password = '', port = 587, secure = false) {
-        if (userName.length > 0 && host.length > 0) {
-            this.#userName = userName
-            this.#host = host
-        } else {
-            this.#userName = null
-            this.#host = null
+        this.#userName = null
+        this.#host = null
+        if (typeof userName !== 'undefined' && typeof host !== 'undefined') {
+            if (userName.length > 0 && host.length > 0) {
+                this.#userName = userName
+                this.#host = host
+            }
         }
         this.#password = password
         this.#port = port
@@ -26,43 +29,68 @@ class Mailer {
     async sendMail(to, subject, html, text = '') {
         
         let rtn = false
-        
-        // TODO: Do not send mails if environment is production
-        
-        if (to.length > 0 && subject.length > 0 && html.length > 0) {
-        
-            if (this.#userName != null && this.#host != null) {
+
+        if (typeof to !== 'undefined' && typeof subject !== 'undefined' && typeof html !== 'undefined') {
+            if (to.length > 0 && subject.length > 0 && html.length > 0) {
+                if (this.#userName != null && this.#host != null) {
+
+                    let sender = this.#userName
+                    if (this.#sender != null) {
+                        sender = this.#sender
+                    }
+                    
+                    // create reusable transporter object using the default SMTP transport
+                    let transporter = mailer.createTransport({
+                        host: this.#host,
+                        port: this.#port,
+                        secure: this.#secure, // true for 465, false for other ports
+                        auth: {
+                            user: this.#userName,
+                            pass: this.#password,
+                        },
+                    });
+                    
+                    // send mail with defined transport object
+                    let info = await transporter.sendMail({
+                        from: sender, // sender address
+                        to: to, // list of receivers (comma separated)
+                        subject: subject, // Subject line
+                        text: text, // plain text body
+                        html: html, // html body
+                        bcc: this.#blindCopy
+                    });
+                    
+                    this.#mailInfo = info
+                    rtn = true
                 
-                // create reusable transporter object using the default SMTP transport
-                let transporter = mailer.createTransport({
-                    host: this.#host,
-                    port: this.#port,
-                    secure: this.#secure, // true for 465, false for other ports
-                    auth: {
-                        user: this.#userName,
-                        pass: this.#password,
-                    },
-                });
-                
-                // send mail with defined transport object
-                let info = await transporter.sendMail({
-                    from: this.#userName, // sender address
-                    to: to, // list of receivers (comma separated)
-                    subject: subject, // Subject line
-                    text: text, // plain text body
-                    html: html, // html body
-                });
-                
-                this.#mailInfo = info
-                
-                rtn = true
-            
+                }
             }
-        
         }
         
         return rtn
         
+    }
+
+    setBlindCopy(blindCopy) {
+        let rtn = false
+        if (typeof blindCopy !== 'undefined') {
+            if (blindCopy.length > 0) {
+                this.#blindCopy = blindCopy
+                rtn = true
+            }
+        }
+        return rtn
+    }
+
+    setSender(sender) {
+        let rtn = false
+        if (typeof sender !== 'undefined') {
+            if (sender.length > 0) {
+                this.#sender = sender
+                rtn = true
+            }
+        }
+        return rtn
     }
 
     getInfo() {
