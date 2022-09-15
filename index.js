a96c381b37ddba1315f79759fb2121ed7f843105
KiraAylaria <74116888+KiraAylaria@users.noreply.github.com>
2022-09-15 17:51:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,5 @@ const mailer = new Mailer('username/mail', 'host', 'pass', 465, true)
 mailTest()
 
 async function mailTest() {
-    let result = await mailer.sendMail('receiver', 'Nodemailer Test', '<p>Nodemailer Test!</p>', 'Nodemailer Test')
-    console.log(mailer.getInfo())
-    return result
+    
 }
